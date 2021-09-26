e2f9fcd6e0d29d10663de8ebd98bc6777d326602
kyehyeok <laaa391919@gmail.com>
2021-09-26 13:02:01 +0900

    startapp profileapp & using modelform

---
+++ profileapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+
+app_name ="profileapp"
+
+urlpatterns = [
+
+]
